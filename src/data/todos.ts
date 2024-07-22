6b387a9a5357d9d7408e308c16bde00a05862842
Markwang-bb <kissmyfacebaby11@gmail.com>
2024-07-23 01:20:15 +0800

    The to-do list is completed

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "gf doi",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Watch Anthony Fu's live stream 📺",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Genshin Impact Launch 😊",
+    completed: false,
+  },
+];
